3bffe9e89d31ef9bdd0de7ad0cd8f246357f9c75
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2016-12-10 01:40:07 -0800

    add some testing starts

---
+++ client/app/lib/crud.spec.js
@@ -0,0 +1,30 @@
+import chai from 'chai';
+chai.should();
+import sinon from 'sinon';
+import Crud from './crud';
+import axios from 'axios';
+
+describe('Client Crud Library', function() {
+  const crud = new Crud();
+
+  beforeEach(function() {
+    sinon.stub(axios, 'get');
+  });
+
+  afterEach(function() {
+  });
+
+  it('should parse fetched data as JSON', function(done) {
+    const data = { foo: 'bar' };
+    const dataJSON = JSON.stringify(data);
+
+    crud.get('/users')
+      .then(function(res) {
+        res.should.deep.equal(data);
+        done();
+      });
+
+    this.requests[0].respond(200, { 'Content-Type': 'text/json' }, dataJSON);
+  });
+
+});
