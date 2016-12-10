dec928606ad1e95a7e1e654e19dbd8de8038e4a6
Luke Golden <webseedsonline@gmail.com>
2016-12-10 08:37:57 -0800

    Add UI components in create-routine

--- client/app/components/routine/create-routine.react.js
+++ client/app/components/routine/create-routine.react.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import CreateRoutineNav from './create-routine-nav.react.js';
+import Paper from 'material-ui/Paper';
+import Divider from 'material-ui/Divider';
 
 export default class CreateRoutine extends React.Component {
   constructor(props) {
@@ -8,9 +11,26 @@ export default class CreateRoutine extends React.Component {
     };
   }
 
+
   render() {
     return (
-      <div></div>
+      <div>
+        <CreateRoutineNav />
+        <Paper style={} zDepth={4} />
+          <TextField
+            onChange={}
+            type="text"
+            hintText="ex. Morning Workout"
+            floatingLabelText="Please input the name of your Routine"
+          /><br />
+          <Divider />
+          // Custom component for Date Repeat integrated with MomentJS?
+          <TextField
+            hintText="ex. My morning workout consisting of stretching, cardio, weightlifting, and some jammin' tunes!" floatingLabelText="Please input the description of your Routine"
+            multiLine={true}
+            rows={3} />
+        <Paper />
+      </div>
     );
   }
 }
