fe296c46b472793207810526bc5db4b4d09fcbbc
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2016-12-10 18:11:20 -0800

    rework the side-menu component to work with the Application component

--- client/app/app.js
+++ client/app/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './components/application/application.react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 ReactDOM.render(
   <div>
-    <Application />
+    <MuiThemeProvider>
+      <Application />
+    </MuiThemeProvider>
   </div>,
   document.getElementById('app-container')
 );
--- client/app/components/side-menu/side-menu.react.js
+++ client/app/components/side-menu/side-menu.react.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import UserStore from '../stores/user-store';
-import UserActions from '../actions/user-actions';
-import RoutineStore from '../stores/routine-store';
-import RoutineActions from '../actions/routine-actions';
-import TaskStore from '../stores/task-store';
-import TaskActions from '../actions/task-actions';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Drawer from 'material-ui/Drawer';
 import Divider from 'material-ui/Divider';
-import List from 'material-ui/svg-icons/social/list';
-import Power from 'material-ui/svg-icons/social/power-settings-new';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import List from 'material-ui/svg-icons/action/list';
+import Power from 'material-ui/svg-icons/action/power-settings-new';
 
 
 export default class SideMenu extends React.Component {
@@ -25,21 +19,18 @@ export default class SideMenu extends React.Component {
     return (
       <div>
         <Drawer
-          open="true"
-          className="">
-          <div>{user.name}</div>
+          open={true} >
+          <div>user</div>
           <Menu>
             <MenuItem
               primaryText="My Routines"
               rightIcon={<List />}
-              onClick={}
             />
             <Divider />
             <MenuItem
               primaryText="Logout"
               rightIcon={<Power />}
-              onClick={}
-             />
+            />
           </Menu>
         </Drawer>
       </div>
