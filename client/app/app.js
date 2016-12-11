4c41baf477f94c9e9f641b8bbedccf009f3e65b3
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2016-12-10 19:19:32 -0800

    remove unneeded dependency from app.js

--- client/app/app.js
+++ client/app/app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './components/application/application.react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 ReactDOM.render(
   <div>
