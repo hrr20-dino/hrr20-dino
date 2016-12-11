401d74a6b1eddada584e0f027374dac61a5d378b
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2016-12-10 16:40:29 -0800

    saving changes before moving to new branch

--- client/app/app.js
+++ client/app/app.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import Application from './components/application/application.react';
 
 ReactDOM.render(
-<div>
-<Application />
-</div>,
+  <div>
+    <Application />
+  </div>,
   document.getElementById('app-container')
 );
