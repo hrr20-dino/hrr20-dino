fa07487d6b47ad6d6234c05a43eb0b77eccbff2c
David Flowers <dvdwdflwrs@gmail.com>
2016-12-11 18:55:52 -0600

    Refactor database_config to inclide History table.  Remove redundant associations

--- database/database_config.js
+++ database/database_config.js
@@ -2,43 +2,47 @@ var Sequelize = require('sequelize');
 
 //#######################__Create Connection__##############################
 
-// var db_connection = new Sequelize('app_data', /*local_username*/, '', {
-//   host: 'localhost',
-//   dialect: 'postgres',
-//   port: /*Environment port*/
-
-var db_connection = new Sequelize('app_data', 'enduser', '', {
+var db_connection = new Sequelize('app_data', /*local username*/, '', {
   host: 'localhost',
   dialect: 'postgres',
-  port: 5432
+  port: /*environment port*/
+
 });
 
 //########################__Define Models__###############################
 
 var User = db_connection.define('user', {
-  name: {type: Sequelize.STRING, unique: true, validate: {notEmpty: true}},
+  name: {type: Sequelize.STRING, unique: true, primaryKey: true, validate: {notEmpty: true}},
   password: {type: Sequelize.STRING, validate: {notEmpty: true}},
   avatar: {type: Sequelize.STRING}
 });
 
 var Routine = db_connection.define('routine', {
-  name: {type: Sequelize.STRING, unique: true, validate: {notEmpty: true}},
+  name: {type: Sequelize.STRING, unique: true, primaryKey: true, validate: {notEmpty: true}},
   description: {type: Sequelize.TEXT, validate: {notEmpty: true}},
-  time_of_day: {type: Sequelize.TIME},
-  repeat: {type: Sequelize.JSON} //this JSON object gets created on the client side
+  start_time: {type: Sequelize.TIME},
+  end_time: {type: Sequelize.TIME},
+  repeat: {type: Sequelize.JSON}, //this JSON object gets created/updated on the client side
+  completed: {type: Sequelize.BOOLEAN}
 });
 
 var Task = db_connection.define('task', {
-  name: {type: Sequelize.STRING, unique: true, validate: {notEmpty: true}},
-  description: {type: Sequelize.STRING, validate: {notEmpty: true}}
+  name: {type: Sequelize.STRING, unique: true, primaryKey: true, validate: {notEmpty: true}},
+  description: {type: Sequelize.STRING, validate: {notEmpty: true}},
+  completed: {type: Sequelize.BOOLEAN}
+});
+
+var History = db_connection.define('history', {
+  date: {type: Sequelize.DATEONLY},
+  completed: {type: Sequelize.BOOLEAN}
 });
 
 //#######################__Define Associations__##############################
 
 User.hasMany(Routine);
-Routine.hasMany(Task);
 Task.belongsTo(Routine);
-Routine.belongsTo(User);
+History.belongsTo(User);
+History.belongsTo(Routine);
 
 //#######################__Sync Database and Export__##############################
 
@@ -48,7 +52,8 @@ module.exports = {
   connection: db_connection,
   User: User,
   Routine: Routine,
-  Task: Task
+  Task: Task,
+  History: History
 }
 
 
