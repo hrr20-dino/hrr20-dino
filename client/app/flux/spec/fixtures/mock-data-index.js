a1fdddfb8472141e82b9659bf670320990dd3dc6
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2016-12-11 23:29:45 -0800

    implement index for mock data

---
+++ client/app/flux/spec/fixtures/mock-data-index.js
@@ -0,0 +1,9 @@
+import MockRoutineData from './mock-routine-data';
+import MockTaskData from './mock-task-data';
+import MockUserData from './mock-user-data';
+
+export default {
+  routine: MockRoutineData,
+  task: MockTaskData,
+  user: MockUserData
+};
