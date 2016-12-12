14c747c2b4cbb62ad7ca1be1a0df42de16625502
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2016-12-11 23:31:10 -0800

    factored out boilerplate in stores, using task-store as a first one to subclass

--- client/app/flux/stores/store.js
+++ client/app/flux/stores/store.js
@@ -4,12 +4,28 @@
 
 
 import EventEmitter from 'events';
+import Crud from '../../lib/crud';
+import MockData from '../spec/fixtures/mock-data-index';
+import _ from 'lodash';
 
 const CHANGE_EVENT = 'change';
 
 class Store extends EventEmitter {
-  constructor() {
+  constructor(subClass) {
     super();
+    this.db = new Crud();
+
+    this.storeName = subClass.storeName;
+    this.mock = false;
+
+    this.data = {
+      collection: []
+    };
+  }
+
+  useMockData() {
+    this.mock = true;
+    this.data.collection = MockData[this.storeName];
   }
 
   emitChange() {
@@ -22,6 +38,64 @@ class Store extends EventEmitter {
 
   removeChangeListener(callback) {
     this.removeListener(CHANGE_EVENT, callback);
+  }
+
+  get(params) {
+    return new Promise((resolve, reject) => {
+      if (this.mock) {
+        resolve(this.data);
+      } else {
+        this.db.get(this.storeName, params)
+          .then((data) => {
+            resolve(data);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      }
+    });
+  }
+
+  add(data) {
+    if (this.mock) {
+      this.data.collection.push(data);
+      this.emitChange();
+    } else {
+      this.db.post(this.storeName, data)
+        .then(() => {
+          this.emitChange();
+        });
+    }
+  }
+
+  update(id, data) {
+    if (this.mock) {
+      const index = _.findIndex(this.data.collection, { id: id });
+      if (index !== -1) {
+        _.assignIn(this.data.collection[index], data);
+        this.emitChange();
+      }
+    } else {
+      this.db.update(`${this.storeName}/${id}`, data)
+        .then(() => {
+          this.emitChange();
+        });
+    }
+  }
+
+  remove(id) {
+    if (this.mock) {
+      const index = _.findIndex(this.data.collection, { id: id });
+      if (index !== -1) {
+        this.data.collection.splice(index, 1);
+        this.emitChange();
+      }
+    } else {
+      this.db.delete(`${this.storeName}/${id}`)
+        .then(() => {
+          this.emitChange();
+        });
+    }
   }
 }
 
--- client/app/flux/stores/task-store.js
+++ client/app/flux/stores/task-store.js
@@ -5,92 +5,32 @@
 import AppDispatcher from '../dispatcher/app-dispatcher';
 import TaskConstants from '../constants/task-constants';
 import Store from './store';
-import Crud from '../../lib/crud';
-import _ from 'lodash';
-import MockTaskData from '../../flux/spec/fixtures/mock-task-data';
+
 
 class TaskStore extends Store {
   constructor() {
-    super();
-
-    this.db = new Crud();
-
-    this.mock = false;
-
-    this.tasks = [];
-  }
-
-  useMockData() {
-    this.mock = true;
-    this.tasks = MockTaskData;
-  }
-
-  getTasks(params = {}) {
-    return new Promise((resolve, reject) => {
-      if (this.mock) {
-        resolve(this.tasks);
-      } else {
-        this.db.get('task', params)
-          .then((data) => {
-            resolve(data);
-          })
-          .catch((err) => {
-            reject(err);
-          });
-      }
+    super({
+      storeName: 'task'
     });
+
+    this.data.tasks = [];
   }
 }
 
-let taskStoreInstance = new TaskStore();
+let that = new TaskStore();
 
-taskStoreInstance.dispatchToken = AppDispatcher.register(action => {
+that.dispatchToken = AppDispatcher.register(action => {
   switch (action.actionType) {
     case TaskConstants.ADD_TASK:
-      if (taskStoreInstance.mock) {
-        taskStoreInstance.tasks.push(action.data);
-        taskStoreInstance.emitChange();
-      } else {
-        taskStoreInstance.db.post('task', action.data)
-          .then(() => {
-            taskStoreInstance.emitChange();
-          });
-      }
-      break;
-
-    case TaskConstants.REMOVE_TASK:
-      if (taskStoreInstance.mock) {
-        const taskIndex = _.findIndex(taskStoreInstance.tasks, (task) => {
-          return task.id === action.id;
-        });
-
-        if (taskIndex !== -1) {
-          taskStoreInstance.tasks.splice(taskIndex, 1);
-          taskStoreInstance.emitChange();
-        }
-      } else {
-        taskStoreInstance.db.delete(`task/${action.data}`)
-          .then(() => {
-            taskStoreInstance.emitChange();
-          });
-      }
+      that.add(action.data);
       break;
 
     case TaskConstants.UPDATE_TASK:
-      if (taskStoreInstance.mock) {
-        const taskIndex = _.findIndex(taskStoreInstance.tasks, (task) => {
-          return task.id === action.id;
-        });
-        if (taskIndex !== -1) {
-          _.assignIn(taskStoreInstance.tasks[taskIndex], action.data);
-          taskStoreInstance.emitChange();
-        }
-      } else {
-        taskStoreInstance.db.update(`task/${action.id}`, action.data)
-          .then(() => {
-            taskStoreInstance.emitChange();
-          });
-      }
+      that.update(action.id, action.newData);
+      break;
+
+    case TaskConstants.REMOVE_TASK:
+      that.remove(action.id);
       break;
 
     default:
@@ -99,4 +39,4 @@ taskStoreInstance.dispatchToken = AppDispatcher.register(action => {
 
 });
 
-export default taskStoreInstance;
+export default that;
