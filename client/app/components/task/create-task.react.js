31e089a85f4a22754cbf32a5c0717441a8ea56bd
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2016-12-12 10:35:03 -0500

    4 component changes for create-task-nav, create-task, task-nav, and task. Cannot view on local so please review ui @luke

--- client/app/components/task/create-task.react.js
+++ client/app/components/task/create-task.react.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import CreateTaskNav from './create-task-nav.react.js';
+import Paper from 'material-ui/Paper';
+import Divider from 'material-ui/Divider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import AddCircleOutline from 'material-ui/svg-icons/content/add-circle-outline';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 export default class CreateTask extends React.Component {
   constructor(props) {
@@ -11,8 +17,55 @@ export default class CreateTask extends React.Component {
   }
 
   render() {
+    const paperStyle = {
+      height: 400,
+      width: 400,
+      margin: 20,
+    };
+    const centerPaper = {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    };
+    const style = {
+      margin: 12,
+    };
     return (
-      <div></div>
+      <div>
+        <CreateTaskNav />
+        <div style={centerPaper}>
+          <div>
+            <Paper style={paperStyle} zDepth={4}>
+              {/* inser onChange into text field */}
+              <div style={{margin: 20}}>
+                <TextField
+                  type="text"
+                  hintText="ex. Morning jog for 30 minutes"
+                  floatingLabelText="Please input the name of your Task"
+                  fullWidth={true}
+                /><br />
+                <Divider />
+                <TextField
+                  hintText="ex. My morning jog consisted of 4 minute intervals with 1 minute rest periods along the San Fransisco Bay" floatingLabelText="Please input the description of your Routine"
+                  fullWidth={true}
+                  multiLine={true}
+                  rows={4} />
+                <RaisedButton
+                  label="Add Another Task"
+                  disabled={true}
+                  style={style} />
+                <RaisedButton
+                  label="Add Task"
+                  labelPosition="before"
+                  primary={true}
+                  icon={<AddCircleOutline />}
+                />
+              </div>
+            </ Paper>
+          </div>
+        </div>
+      </div>
+
     );
   }
 }
--- client/app/components/task/task.react.js
+++ client/app/components/task/task.react.js
@@ -5,6 +5,11 @@ import RoutineStore from '../stores/routine-store';
 import RoutineActions from '../actions/routine-actions';
 import TaskStore from '../stores/task-store';
 import TaskActions from '../actions/task-actions';
+import TaskNav from './task-nav.react.js';
+import Paper from 'material-ui/Paper';
+import {List, ListItem} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import Launch from 'material-ui/svg-icons/file/launch';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
@@ -17,8 +22,34 @@ export default class Task extends React.Component {
   }
 
   render() {
+    const paperStyle = {
+      height: 400,
+      width: 400,
+      margin: 20,
+    };
+    const centerPaper = {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    };
     return (
-      <div></div>
+      <div>
+        <TaskNav />
+        <div style={centerPaper}>
+          <div>
+            <Paper style={paperStyle} zDepth={4}>
+              <List>
+                {/*for each task in routine*/}
+                <ListItem
+                  primaryText="Test"
+                  rightIcon={<Launch />}
+                />
+              </List>
+              <Divider />
+            </Paper>
+          </div>
+        </div>
+      </div>
     );
   }
 }
