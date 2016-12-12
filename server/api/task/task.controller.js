9985ef187f9f8d748f0c01cc15ac682de6c7fca7
Alonzo Alden <alonzoalden@gmail.com>
2016-12-11 16:08:47 -0800

    updates

---
+++ server/api/task/task.controller.js
@@ -0,0 +1,33 @@
+<<<<<<< 73bf1f069358bdfa1e6f8ef7a8a487f0fc33f09a
+=======
+var Promisify = require('bluebird');
+var Models = require('/Users/enduser/desktop/hrr/hrr20-dino/database/database_config.js');
+//find a way to make that a better require location. why won't ../../database/database_config.js work?
+
+module.exports = {
+
+  function: function() {
+    console.log('HELLLLOOO WORLD')
+  },
+
+  addTask: function (req, res, next) {
+
+  },
+
+  //gets the routines for the specific user
+  getAllTasks: function(req, res, next) {
+
+  },
+
+  getTask: function(req, res, next) {
+    var id = req.params.task_id
+    Models.Task.findOne({
+      where: {
+        id: Number(req.params.id)
+      }
+    })
+  },
+
+
+}
+>>>>>>> updates
