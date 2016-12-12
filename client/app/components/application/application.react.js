9aa29ef0f189b8c54110e918aef64ed627f7ea6b
Luke Golden <webseedsonline@gmail.com>
2016-12-11 21:11:02 -0800

    Take 3...

--- client/app/components/application/application.react.js
+++ client/app/components/application/application.react.js
@@ -1,10 +1,6 @@
 import React from 'react';
-<<<<<<< a323ff5f9202d347e66a26cb031bef0ea6e3fec0
-// import SideMenu from '../../components/side-menu/side-menu.react';
 import TestComponent from './test-component.react';
-=======
 import PreAuthNav from '../routine/pre-auth-nav.react';
->>>>>>> Updated UI components for Routine views and added Pre-Auth Nav
 
 export default class Application extends React.Component {
   constructor(props) {
@@ -14,14 +10,8 @@ export default class Application extends React.Component {
   render() {
     return (
       <div id='application'>
-<<<<<<< a323ff5f9202d347e66a26cb031bef0ea6e3fec0
-        {/*<SideMenu />*/}
+        {/*<PreAuthNav />*/}
         <TestComponent />
-=======
-        {/*}<SideMenu />*/}
-        <PreAuthNav />
-        {/*}<CreateRoutine />*/}
->>>>>>> Updated UI components for Routine views and added Pre-Auth Nav
       </div>
     );
   }
