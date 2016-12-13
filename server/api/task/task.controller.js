5754b1d120aa3e83828792b21e0a94324404622d
Alonzo Alden <alonzoalden@gmail.com>
2016-12-12 17:15:20 -0800

    Updated task controller and routes

--- server/api/task/task.controller.js
+++ server/api/task/task.controller.js
@@ -1,17 +1,15 @@
 const Promise = require('bluebird');
-const Models = Promise.promisifyAll(require('../../../database/database_config').User);
+const Models = require('../../../database/database_config');
 
 
 module.exports = {
 
   addTask: function (req, res, next) {
-<<<<<<< 4634c8679a039d208c97d5f566a1f2d8f9896f5a
-      if (JSON.parse(req.body))
-      var task = req.body
-=======
-    var task = JSON.parse(req.body);
-    if (t)
-    Models.Task.build(req.body)
+    Models.Task.build({
+      name: req.body.name,
+      description: req.body.description,
+      completed: req.body.completed
+    })
     .save()
     .then(function(addedTask) {
       res.json(addedTask.dataValues);
@@ -19,29 +17,50 @@ module.exports = {
     .catch(function(error) {
       next(error);
     });
->>>>>>> Updated task controller and routes
   },
 
   //gets the routines for the specific user
   getAllTasks: function(req, res, next) {
-
-  },
-
-  getATask: function(req, res, next) {
-    var id = req.params.task_id
-    Models.Task.findOne({
+    Models.Task.findAll({
       where: {
-        id: Number(req.params.id)
+        id: req.params.routineId
       }
     })
+      .then(function (tasks) {
+        res.json(tasks);
+      })
+      .catch(function(error) {
+        next(error);
+      });
+
   },
 
   deleteATask: function(req, res, next) {
-
+    Models.Task.destroy({
+      where: {
+        id: req.params.taskName
+      }
+    })
+    .then(function(result) {
+      res.json(result);
+    })
+    .catch(function(error) {
+      next(error);
+    })
   },
 
   updateATask: function(req, res, next) {
-
+    Models.Task.update(req.body, {
+      where: {
+        id: Number(req.params.taskName)
+      }
+    })
+    .then(function(result) {
+      res.json(result[0]);
+    })
+    .catch(function(error) {
+      next(error);
+    })
   }
 
 
