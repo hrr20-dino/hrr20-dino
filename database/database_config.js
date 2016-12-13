a70d083b86f594616b710d626e2ed3556793d711
Alonzo Alden <alonzoalden@gmail.com>
2016-12-13 15:26:49 -0800

    set up databse_config for deployment

--- database/database_config.js
+++ database/database_config.js
@@ -2,7 +2,7 @@ var Sequelize = require('sequelize');
 
 //#######################__Create Connection__##############################
 
-var db_connection = new Sequelize('app_data', 'postgres', '', {
+var db_connection = new Sequelize('app_data', 'root', '', {
   host: 'localhost',
   dialect: 'postgres',
   port: 5432
